4382ecd61fbb3e6fef7b22bd2a473ee837a76d56
Umanga Shrestha <umangashrestha93@gmail.com>
2023-07-27 14:38:44 +0545

    displayed products to cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import './cart.css'
 
 function Cart() {
+  const cardItem = useSelector((state)=>state.cart)
   return (
-    <div>
-        <h1>cart</h1>
-      
+    <div className='cardWrapper'>
+      {
+        cardItem.map((item)=>(
+          <div className='cardItem'>
+            <img src={item.image} alt='img'/>
+            <h5>{item.title}</h5>
+            <h5>{item.price}</h5>
+            <button className='btn-1'>remove</button>
+          </div>
+        ))
+      }
     </div>
   )
 }
